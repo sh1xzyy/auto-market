1b12eea7f7e05a62516775555ae81f1900a732be
sh1xzyy <alekslungol@gmail.com>
2025-11-22 18:24:14 +0300

    feat(header): add notification and burger menu btn

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,9 +4,16 @@ import { useWindowWidth } from "@/shared/hooks/useWindowWidth";
 import { Logo } from "./ui/Logo/Logo";
 import { DefaultLink } from "@/shared/ui/Link/DefaultLink";
 import { routes } from "@/shared/constants/routes";
+import { IconButton } from "@/shared/ui/IconButton/IconButton";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaRegBell } from "react-icons/fa";
+import { usePathname } from "next/navigation";
 
 export const Header = () => {
   const { windowWidth } = useWindowWidth();
+  const pathname = usePathname();
+  const isPathAuth = pathname === "/auth";
+
   return (
     <>
       <div className="flex items-center h-[72px] bg-background-light-black">
@@ -14,7 +21,24 @@ export const Header = () => {
           <nav className="flex items-center justify-between">
             <Logo />
 
-            {windowWidth > 755 && (
+            {windowWidth <= 755 && !isPathAuth && (
+              <div>
+                {windowWidth > 575 && (
+                  <IconButton
+                    icon={FaRegBell}
+                    className="p-[9px]"
+                    ariaLabel="Notifications button"
+                  />
+                )}
+                <IconButton
+                  icon={GiHamburgerMenu}
+                  className="p-[9px]"
+                  ariaLabel="Burger Menu button"
+                />
+              </div>
+            )}
+
+            {windowWidth > 755 && !isPathAuth && (
               <>
                 <div className="flex items-center">
                   <DefaultLink
---
+++ src/shared/ui/IconButton/IconButton.jsx
@@ -0,0 +1,29 @@
+import clsx from "clsx";
+
+export const IconButton = ({
+  icon: Icon,
+  size = 20,
+  onClick,
+  className = "",
+  iconClassName = "group-hover:fill-light-white",
+  ariaLabel,
+  children,
+}) => {
+  return (
+    <button
+      className={clsx("group transition-colors duration-300", className)}
+      type="button"
+      onClick={onClick}
+      aria-label={ariaLabel}
+    >
+      <Icon
+        className={clsx(
+          "fill-light-grey transition-colors duration-300",
+          iconClassName
+        )}
+        size={size}
+      />
+      {children}
+    </button>
+  );
+};
