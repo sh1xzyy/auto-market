8cd255780825d9a5785e94ed33960209c395e093
sh1xzyy <alekslungol@gmail.com>
2025-11-27 01:38:10 +0300

    feat(ai_helper_intro): add AIHelperIntro component and add some condition styles for DefaultLink

--- src/shared/ui/ContentCard/ContentCard.jsx
+++ src/shared/ui/ContentCard/ContentCard.jsx
@@ -3,6 +3,7 @@ import clsx from "clsx";
 export const ContentCard = ({ children, variant }) => {
   const classByVariant = {
     base: "p-4 bg-background-light-black border-t border-b border-grey lg:border lg:rounded-lg",
+    primary: "lg:p-[24px]",
   };
 
   return (
--- src/shared/ui/Link/DefaultLink.jsx
+++ src/shared/ui/Link/DefaultLink.jsx
@@ -2,17 +2,18 @@ import Link from "next/link";
 
 export const DefaultLink = ({ variant, href, Icon, text, ...props }) => {
   const centeredStyles = "flex items-center justify-center w-full";
+  const transitionStyles = "transition linear duration-300";
 
   const variants = {
-    primary: `${centeredStyles} h-[36px] px-[12px] font-bold rounded-md text-[14px] bg-light-violet transition linear duration-300 hover:bg-dark-violet`,
-    secondary: `${centeredStyles} gap-2 rounded-md h-[44px] text-[16px] bg-light-orange transition-colors linear duration-300 hover:bg-dark-orange`,
+    primary: `${centeredStyles} gap-2 h-[36px] px-[12px] font-bold rounded-md text-[14px] bg-light-violet ${transitionStyles} hover:bg-dark-violet`,
+    secondary: `${centeredStyles} gap-2 rounded-md h-[44px] text-[16px] bg-light-orange ${transitionStyles} hover:bg-dark-orange`,
     default: "group block p-[9px]",
   };
 
   return (
     <Link href={href} className={variants[variant]} {...props}>
-      {Icon && <Icon size={16} />}
-      {text && <span>{text}</span>}
+      {Icon && Icon}
+      {text && <span className="text-md">{text}</span>}
     </Link>
   );
 };
