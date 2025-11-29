4d616ab4eb03de5df1406926a3fe9c3dc339a454
sh1xzyy <99531559+sh1xzyy@users.noreply.github.com>
2025-11-29 15:56:51 +0300

    feat(ui): refactor buttons primary style

--- src/shared/ui/Button/Button.jsx
+++ src/shared/ui/Button/Button.jsx
@@ -12,7 +12,7 @@ export const Button = ({
   const transitionStyles = "transition-colors linear duration-300";
 
   const variants = {
-    primary: `h-[44px] text-light-white bg-light-orange rounded-md ${transitionStyles}hover:bg-dark-orange`,
+    primary: `h-[36px] text-light-white bg-light-orange rounded-md ${transitionStyles} hover:bg-dark-orange`,
     primary_big: `w-full h-[52px] px-[24px] bg-light-orange rounded-md sm:px-[16px] sm:py-[12px] sm:h-[44px] ${transitionStyles} hover:bg-dark-orange`,
     secondary: `rounded-md border border-super-light-violet text-super-light-violet h-[44px] ${transitionStyles} hover:border-light-violet hover:text-light-violet`,
     tag: `flex items-center justify-center w-full h-9 px-3 py-2 rounded-xs ${transitionStyles} ${condition}`,
