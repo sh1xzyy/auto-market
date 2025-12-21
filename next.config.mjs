8027be4462c51eb5879b9ec47c1f4b4af70532a4
wortyx <alekslungol@gmail.com>
2025-12-21 23:13:55 +0200

    add project settings

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["img.classistatic.de", "placehold.co", "static.classistatic.de"],
   },
+  watchOptions: {
+    ignored: ['**/node_modules/**', '**/.next/**'],
+  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default nextConfig;
